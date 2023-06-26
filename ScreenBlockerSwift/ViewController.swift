ffa0109438cafb90055cc7358161ed9d7bcd1775
BC <bruce12182004@gmail.com>
2023-06-26 14:33:24 +0800

    Initial Commit

---
+++ ScreenBlockerSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenBlockerSwift
+//
+//  Created by Mitake on 2023/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
